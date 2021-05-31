ac51e6907a11ba1864d45f88e9ef1278ac5c7bcf
Tim Grein <tim@4greins.de>
2021-05-31 12:42:40 +0200

    Fix AddEdgeToChildNode by using smart pointers and move semantics when calling the method instead of raw pointers.

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -27,7 +27,7 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.emplace_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
     _childEdges.emplace_back(std::move(edge));
 }
--- src/graphnode.h
+++ src/graphnode.h
@@ -1,6 +1,7 @@
 #ifndef GRAPHNODE_H_
 #define GRAPHNODE_H_
 
+#include <memory>
 #include <vector>
 #include <string>
 #include "chatbot.h"
@@ -44,7 +45,7 @@ public:
     // proprietary functions
     void AddToken(std::string token); // add answers to list
     void AddEdgeToParentNode(GraphEdge *edge);
-    void AddEdgeToChildNode(GraphEdge *edge);
+    void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
 
     //// STUDENT CODE
     ////
